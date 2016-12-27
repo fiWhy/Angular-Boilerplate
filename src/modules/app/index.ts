330bf163d49a9a5261d8216c3fce3c41db8e7460
Buryachkovsky Denis <fiwhy@mail.ru>
2016-12-27 22:10:31 +0200

    Removed typings

--- src/modules/app/index.ts
+++ src/modules/app/index.ts
@@ -1,6 +1,6 @@
 import {routes} from './config/route';
 import {AppService} from './services/app';
 
-export default angular.module('app.modules.init', [])
+export default angular.module('app.modules.app', [])
     .service('AppService', AppService)
     .config(routes);
--- src/modules/app/tests/index.spec.ts
+++ src/modules/app/tests/index.spec.ts
@@ -1,11 +1,11 @@
-describe("First app testing", function () {
+describe("First app testing",  () => {
     let AppService;
     beforeEach(angular.mock.module('app'));
-    beforeEach(angular.mock.inject((_AppService_) => {
+    beforeEach(angular.mock.inject(function(_AppService_) {
         AppService = _AppService_;
     }))
 
-    it("should check for data", function () {
+    it("should check for data",  () => {
         let data = 'Hello World!';
         expect(AppService.checkForData(data)).toBe(data);
     });
